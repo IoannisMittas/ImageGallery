09dc3015ad800c6040977a46d1d358050fcd69be
Yannis <mittas.ioannis@gmail.com>
2018-05-14 17:43:58 +0300

    filled ImageListViewModel

--- app/src/main/java/com/mittas/imagegallery/viewmodel/ImageListViewModel.java
+++ app/src/main/java/com/mittas/imagegallery/viewmodel/ImageListViewModel.java
@@ -1,4 +1,36 @@
 package com.mittas.imagegallery.viewmodel;
 
-public class ImageListViewModel {
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MediatorLiveData;
+import android.support.annotation.NonNull;
+
+import com.mittas.imagegallery.BasicApp;
+import com.mittas.imagegallery.data.ImageModel;
+
+import java.util.List;
+
+public class ImageListViewModel extends AndroidViewModel{
+
+    private final MediatorLiveData<List<ImageModel>> observableImages;
+
+    public ImageListViewModel(@NonNull Application application) {
+        super(application);
+
+        observableImages = new MediatorLiveData<>();
+
+        LiveData<List<ImageModel>> images = ((BasicApp) application).getRepository()
+                .getAllImages();
+
+        // observe the changes of the images from the database and forward them
+        observableImages.addSource(images, observableImages::setValue);
+    }
+
+    /**
+     * Expose the LiveData images query so the UI can observe it.
+     */
+    public LiveData<List<ImageModel>> getAllImages() {
+        return observableImages;
+    }
 }
